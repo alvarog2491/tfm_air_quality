40fde76525366c07f9e9d22bb34ebcb7e23801f2
alvarog2491 <159990212+alvarog2491@users.noreply.github.com>
2025-07-03 08:02:58 +0200

    added new tests to test_province_mapper

--- src/dataset_creator/tests/test_province_mapper.py
+++ src/dataset_creator/tests/test_province_mapper.py
@@ -5,42 +5,51 @@ from pathlib import Path
 from pandas.api.types import is_categorical_dtype
 from utils.province_mapper import ProvinceMapper
 
-
 # Mock logger to suppress real logging
 class MockLogger:
     def info(self, msg):
+        pass
+
+    def warning(self, msg):
         pass
 
 
 @pytest.fixture(autouse=True)
 def setup_mocks(monkeypatch):
     ProvinceMapper.logger = MockLogger()
-    
-    # Mock _load_json_file and _check_provinces as no-ops
-    monkeypatch.setattr(ProvinceMapper, "_load_json_file", lambda: None)
-    monkeypatch.setattr(ProvinceMapper, "_check_provinces", lambda df: None)
 
-
-@pytest.fixture(autouse=True)
-def real_province_mapping():
-    json_path = Path(__file__).parent.parent / 'utils' / 'unified_province_name.json'
-    if not json_path.is_file():
-        raise FileNotFoundError(f"Expected file not found: {json_path}")
-    
-    with open(json_path, "r", encoding="utf-8") as f:
-        ProvinceMapper.unified_province_dict = json.load(f)
-
-
-def test_map_province_name_success():
-    df_input = pd.DataFrame({
+@pytest.fixture()
+def df_input():
+    return pd.DataFrame({
         "Province": ["CiudadReal", "Palmas,Las"]
     })
 
-    expected = pd.DataFrame({
+@pytest.fixture()
+def df_expected():
+    return pd.DataFrame({
         "Province": pd.Categorical(["Ciudad Real", "Las Palmas"])
     })
 
+def test_map_province_name_success(df_input, df_expected):
+    """Test basic functionallity maping provinces"""
+    
     result = ProvinceMapper.map_province_name("Test DF", df_input)
+    pd.testing.assert_series_equal(result["Province"], df_expected["Province"])
 
-    pd.testing.assert_series_equal(result["Province"], expected["Province"])
-    assert is_categorical_dtype(result["Province"])
+def test_correct_type_province_type(df_input):
+    """After mapping the province, the df province variable should be transformed to category"""
+
+    result = ProvinceMapper.map_province_name("Test DF", df_input)
+    assert isinstance(result["Province"].dtype, pd.CategoricalDtype)
+
+def test_map_non_existing_province(df_input):
+    """If a province is unrecognized, it should not be mapped by anything"""
+    unknown_province = pd.DataFrame({"Province": ["something"]})
+    df_input = pd.concat([df_input, unknown_province])
+    
+    result = ProvinceMapper.map_province_name("Test DF", df_input)
+    
+    assert "something" in result["Province"].values
+
+
+
