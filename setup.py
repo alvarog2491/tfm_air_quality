5cd34ec027d0b8ade2cf53fafd2bbd8f446e302f
alvarog2491 <159990212+alvarog2491@users.noreply.github.com>
2025-07-24 11:38:58 +0200

    formated setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='tfm_air_quality',
-    version='0.1',
-    packages=find_packages(where='src'),
-    package_dir={'': 'src'},
+    name="tfm_air_quality",
+    version="0.1",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
 )
